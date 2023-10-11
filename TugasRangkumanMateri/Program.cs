b93a3bfd31b13ae97b03c56d07b5391e2ccf3a1a
Redhie <147617708+Redhie@users.noreply.github.com>
2023-10-11 19:21:33 +0700

    Tugas DNCC Game Bhima Pradita A11.2023.14966
    lelah

---
+++ TugasRangkumanMateri/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+namespace ringkasanMateri;
+
+/*
+//Jika ingin menjalankan program dibawah bisa menggunakan komentar
+class Pertama {
+    private static void Main(string[] args){
+        int angka1 = 8; //variable menggunakan tipe int (angka)
+        float angka2 = 3.6f;//variable menggunakan tipe float (7 digit desimal)
+        double angka3 = 4.2; //variable menggunakan tipe double (15 digit desimal)
+        string kata = "haii";//variable menggunakan tipe string (kalimat)
+        string angka4 = "2";//variable menggunakan tipe string (angka)
+        bool kebenaran = true;//variable menggunakan tipe bool (true/false)
+        var rand = 4;
+        const float phi = 3.14f;
+
+        string newLine = "\r"; //digunakan untuk membuat baris baru 
+        string tab = "\t"; //digunakan untuk membuat kolom baru
+        
+        string namaDepan = "Bhima";
+        string namaBelakang = "Pradita";
+        string namaLengkap = namaDepan + " " + namaBelakang;
+
+
+        Console.WriteLine(angka1);
+        Console.WriteLine(angka2);
+        Console.WriteLine(angka3);
+        Console.WriteLine(kata);
+        Console.WriteLine(angka4);
+        Console.WriteLine(kebenaran);
+        Console.WriteLine(rand);
+        Console.WriteLine(phi);
+        Console.WriteLine(namaLengkap);
+
+        //penjumlahan
+    
+        int hasil1 = 1 + 1; //operator penjumlahan
+        int hasil2 = 2 - 1; //operator pengurangan
+        int hasil4 = 2 * 2; //operator perkalian
+        int hasil5 = 4 / 2; //operator pembagian
+        int hasil6 = 5 % 2; //operator modulus (sisa bagi)
+
+        Console.WriteLine($"{hasil1}. {hasil2}. {hasil4}. {hasil5}. {hasil6}");
+    }
+}
+*/
+
+
+        //operator logika
+
+        /*
+        Operator logika ada 3 macam
+        - && untuk "dan"
+        - || untuk "atau"
+        - ! untuk "negasi/kebalikan"
+        */
+
+
+    //array
+
+//Array digunakan untuk menyimpan koleksi elemen dengan tipe data yang sama dalam urutan terindeks.
+//Indeks array dimulai dari 0, yang berarti elemen pertama memiliki indeks 0, elemen kedua memiliki indeks 1, dan seterusnya.
+
+
+//class Kedua{
+    //static void Main(string[] args){
+        /*
+        string[] divisi = { "web", "mobile", "game", "mulmed", "jaringan"};
+        Console.WriteLine(divisi[0]);
+        Console.WriteLine(divisi[1]);
+        Console.WriteLine(divisi[2]);
+        Console.WriteLine(divisi[3]);
+        Console.WriteLine(divisi[4]);
+
+        string[] names = new string[5];
+        names[0] = "web";
+        names[0] = "mobile";
+        names[0] = "game";
+        names[0] = "mulmed";
+        names[0] = "jaringan";
+        */
+
+        /*
+        //Array 2D
+        int[,] number = new int[2,3];
+        int[,] numbers = { {1, 2, 3}, { 4, 5, 6} };
+        Console.WriteLine(numbers[0, 1]); //output 2
+        Console.WriteLine(numbers[1, 2]); //output 6
+        */
+
+        /*
+        //Array 3D
+        int[,,] number = new int[2, 2, 3];
+        int[,,] numbers = { { {1, 2, 3}, {4, 5, 6} }, { {7, 8, 9}, {10, 11, 12} } };
+        Console.WriteLine(numbers[0, 0, 0]); //output 1
+        Console.WriteLine(numbers[1, 1, 0]); //output 10
+        */
+    //}
+    
+//}
+
+    //List
+
+//List adalah salah satu struktur data yang sangat berguna dan fleksibel yang digunakan untuk menyimpan kumpulan elemen atau objek dalam urutan tertentu.
+//List ini termasuk dalam namespace System.Collections.Generic.
+//List memungkinkan Anda untuk menambahkan, menghapus, atau mengakses elemen-elemen di dalamnya dengan mudah.
+
+/*
+class Ketiga{
+            List<int> angka = new List<int>();
+
+            //menambahkan data pada list
+            angka.Add(1);
+            angka.Add(2);
+            angka.Add(3);
+
+            //menambahkan sekaligus beberapa data pada list
+            angka.AddRange(new int[3] { 4, 5, 6 });
+
+            //menghapu data/ item pada list
+            angka.Remove(1);
+
+            //menghapus data pada list berdasarkan index
+            angka.RemoveAt(0);
+
+            //mengetahui index pada suatu data
+            angka.IndexOf(3);
+
+            //Mengecek apakah ada data yang sesuai
+            Console.WriteLine(angka.Contains(3));
+
+            //Mengecek panjang suatu list
+            Console.WriteLine(angka.Count());
+            
+            foreach (var a in angka)
+            {
+                Console.WriteLine(a + " ");
+            }
+        */
+
+
+
+
